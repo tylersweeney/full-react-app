ed3a99b24fd99a147668cfe122298a857fce3813
Tyler <tylersweeney@gmail.com>
2018-12-05 19:30:34 -0800

    deconstruct ninjas component

--- myapp/src/ninjas.js
+++ myapp/src/ninjas.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        // console.log(this.props);
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (            
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-
-            </div>
-            )
-        })
-        return(
-            <div className="ninja-list">
-               { ninjaList } 
-            </div>
+const Ninjas = ({ninjas}) => {
+    // console.log(this.props);
+    const ninjaList = ninjas.map(ninja => {
+        return (            
+        <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+        </div>
         )
-    }
+    })
+    return(
+        <div className="ninja-list">
+            { ninjaList } 
+        </div>
+    )
 }
 
 export default Ninjas
