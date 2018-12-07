5de54ab9beb47454e70139ed07fa02f32c9a94f2
Tyler <tylersweeney@gmail.com>
2018-12-06 19:14:17 -0800

    add react router

---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => }
+return (
+    <div className="container">
+        <h4 className="center">Home</h4>
+        <p>
+            Here is a little paragraph just for fun
+        </p>
+    </div>
+)
+
+export default Home
