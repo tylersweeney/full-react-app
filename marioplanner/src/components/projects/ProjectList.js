e4619190e5a95065c1fae89b57791f4195f4e917
Tyler <tylersweeney@gmail.com>
2018-12-08 13:49:13 -0800

    create ProjectSummary component and nest inside of ProjectList component

--- marioplanner/src/components/projects/ProjectList.js
+++ marioplanner/src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-        <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by Tyler Sweeney</p>
-                <p className="grey-text">8th December, 1:30PM</p>
-            </div>
-        </div>
-
-       <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by Tyler Sweeney</p>
-                <p className="grey-text">8th December, 1:30PM</p>
-            </div>
-        </div>
-
-               <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by Tyler Sweeney</p>
-                <p className="grey-text">8th December, 1:30PM</p>
-            </div>
-        </div>
-
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ marioplanner/src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                    <p>Posted by Tyler Sweeney</p>
+                    <p className="grey-text">8th December, 1:30PM</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
