6095ec5d24e0ed10539ba97c524f5001f55bebaa
Tyler <tylersweeney@gmail.com>
2018-12-08 13:21:35 -0800

    create SignedInLinks component

--- marioplanner/src/App.js
+++ marioplanner/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-route-dom'
+import { BrowserRouter } from 'react-router-dom'
+import NavBar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>MarioPlan</h1>
+          <NavBar />
         </div>
       </BrowserRouter>
     );
--- marioplanner/src/components/layout/Navbar.js
+++ marioplanner/src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
 
 const Navbar = () => {
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
             <Link to='/' className="brand-logo">MarioPlan</Link>
+            <SignedInLinks />
             </div>
         </nav>
     )
