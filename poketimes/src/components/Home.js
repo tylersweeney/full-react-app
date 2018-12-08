e94024feed7c905beafb86fa18e15520e208d95d
Tyler <tylersweeney@gmail.com>
2018-12-07 20:04:36 -0800

    poketime routes

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component{
     state = {
@@ -20,8 +22,11 @@ class Home extends Component{
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="A Pokeball"/>
                         <div className="card-content">
-                            <span className="card-title">{post.title}</span>
+                            <Link to={'/' + post.id}>
+                                <span className="card-title red-text">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -34,21 +39,6 @@ class Home extends Component{
             <div className="container">
                 <h4 className="center">Home</h4>
                 {postList}
-                {/* <p>
-                My name is Tyler Sweeney and I am a Full-Stack Web Developer.
-                I have 3 years experience building full application front to back. 
-                I specialize in MERN stack builds, but am experienced in many coding languages. 
-                I also have experience with SEO and online advertising.
-        
-                I began programming in 2015 with two Python classes at Orange Coast College. 
-                I completed my Full-Stack Web Developer certification in April 2018. 
-                A gamer since a young age when every game had to be launched from DOS, 
-                my first interaction with programming was terminal commands.
-        
-                In my free time I have been learning C++ and the Unreal Engine. 
-                Industries that I am passionate about being a part of are 
-                Gaming, Music, Entertainment, Business, Real Estate, and Sports.
-                </p> */}
             </div>
         )
     }
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink, withRouter } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
     // setTimeout(() => {
@@ -8,11 +8,11 @@ const Navbar = (props) => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a className="brand-logo">Poke' Times</a>
+                <a href="/" className="brand-logo">Poke' Times</a>
                 <ul className="right">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
