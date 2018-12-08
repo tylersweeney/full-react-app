4f6ab6f5e832731ed7df40c8c0ee1f2b0bd4d48f
Tyler <tylersweeney@gmail.com>
2018-12-08 14:04:39 -0800

    create ProjectDetails component

--- marioplanner/src/App.js
+++ marioplanner/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
-import ProjectDeatils from './components/projects/ProjectDetails'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <Switch>
-            <Route path='/' component={Dashboard} />
+            <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
           </Switch>
         </div>
