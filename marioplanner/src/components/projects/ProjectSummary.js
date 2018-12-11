d39a027d2f8d5d507363298fb2fe5a933d2fa5f8
Tyler <tylersweeney@gmail.com>
2018-12-10 18:14:12 -0800

    output dynamically project title in ProjectSummary component

--- marioplanner/src/components/projects/ProjectSummary.js
+++ marioplanner/src/components/projects/ProjectSummary.js
@@ -4,7 +4,7 @@ const ProjectSummary = () => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                     <p>Posted by Tyler Sweeney</p>
                     <p className="grey-text">8th December, 1:30PM</p>
             </div>
