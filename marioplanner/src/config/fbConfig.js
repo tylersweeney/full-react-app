dae3d0b2b8748015dee214e1c9a166fb14076dec
Tyler <tylersweeney@gmail.com>
2018-12-10 19:01:00 -0800

    create firebaseConfig component

---
+++ marioplanner/src/config/fbConfig.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCdiAoCBohitrcIwLzkGdQlBFELHRyGIiw",
+    authDomain: "marioplan-9ca05.firebaseapp.com",
+    databaseURL: "https://marioplan-9ca05.firebaseio.com",
+    projectId: "marioplan-9ca05",
+    storageBucket: "marioplan-9ca05.appspot.com",
+    messagingSenderId: "1083563287014"
+  };
+  firebase.initializeApp(config);
