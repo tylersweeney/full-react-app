d6f04b148e0345784bd2676e264b3af963c3f4d5
Tyler <tylersweeney@gmail.com>
2018-12-10 18:51:36 -0800

    create a dispatch for CREATE_PROJECT

--- marioplanner/src/components/projects/ProjectSummary.js
+++ marioplanner/src/components/projects/ProjectSummary.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
