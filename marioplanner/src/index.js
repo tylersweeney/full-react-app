635edcfb540d84f2b0a6bbcc4f93cd53a3ba19ad
Tyler <tylersweeney@gmail.com>
2018-12-10 20:18:25 -0800

    add reduxFirestore and reactReduxFirebase libraries to index.js

--- marioplanner/src/index.js
+++ marioplanner/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import {Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import {getFirestore} from 'redux-firestore'
-import { getFirebase } from 'react-redux-firebase'
+import {reduxFirestore, getFirestore} from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 
-const store =createStore(rootReducer, applyMiddleware(thunk));
+const store =createStore(rootReducer, 
+    compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(),
+    reactReduxFirebase(),
+    )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 // serviceWorker();
