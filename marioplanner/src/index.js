782e378e3b1770bd729b1759823f702cddea3972
Tyler <tylersweeney@gmail.com>
2018-12-10 20:31:59 -0800

    add CREATE_PROJECT_ERROR to projectReducer case

--- marioplanner/src/index.js
+++ marioplanner/src/index.js
@@ -9,12 +9,13 @@ import {Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import {reduxFirestore, getFirestore} from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
 const store =createStore(rootReducer, 
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(),
-    reactReduxFirebase(),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig),
     )
 );
 
--- marioplanner/src/store/actions/projectActions.js
+++ marioplanner/src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
         //make async call to database
-        dispatch({ type: 'CREATE_PROJECT', project });
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Tyler',
+            authorLastName: 'Sweeney',
+            authorId: 4242,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch(() => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err});
+        })
+        
     }
 };
